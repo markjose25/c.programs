ba9929d46bedf0d82e0b532f15ed4b2d49238bb7
mark joseph <mj2770588@gmail.com>
2025-09-25 07:23:56 -0700

    Add files via upload

---
+++ Bank loan.c
@@ -0,0 +1,31 @@
+// surface area and volume 
+/*
+Mark Joseph wanyeri 
+REG.CT101/G/26443/25
+date 9/23/2025
+calculations of volume and surface area 
+*/
+#include<stdio.h> 
+
+int main()
+{
+//variable declaration 
+int age;
+float  salary ;
+
+//prompt user to enter age 
+printf("Enter your age :");
+scanf("%d",&age);
+ 
+ //prompt user to enter salary 
+ printf("Enter your  annual salary");
+ scanf("%f",&salary);
+
+//check eligibility 
+if(age>=21 && salary >=21000){
+printf("congratulations you are qualified for the loan .\n");
+}else{
+printf("unfortunately we are unableto give you a lone at this time .\n");
+}
+      return 0;
+}
